ff7aaa0e04951155e0cdf4e4330e1a1e606fd5a5
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-01-04 11:22:50 -0600

    more TestNG practice

---
+++ src/TestNG/SampleClass.java
@@ -0,0 +1,12 @@
+package TestNG;
+
+public class SampleClass {
+    public String mergeStrings(String str1, String str2)
+    {
+        return str1.concat(str2);
+    }
+    public String mergeStringsWithDash(String str1, String str2)
+    {
+        return str1.concat("-").concat(str2);
+    }
+}
